24438d3530372eb4fb98e0bb762bfe903b15526d
Andrea <andybbruno@gmail.com>
2020-01-02 16:00:01 +0100

    Fixed

--- undersamplingDF.py
+++ undersamplingDF.py
@@ -1,9 +1,18 @@
 from imblearn.under_sampling import RandomUnderSampler
 import pandas as pd
 
-file = "train.csv"
+file = "kids_train_cleaned.csv"
 df = pd.read_csv(file)
+df['WheelType'].fillna('UNKNOWN', inplace=True)
 
 y = df['IsBadBuy']
 X = df.drop(columns=['IsBadBuy'])
+
+cols = X.columns
 X, y = RandomUnderSampler().fit_resample(X, y)
+
+
+new = pd.DataFrame(data=X, columns=cols)
+new_y = pd.DataFrame(data=y, columns=['IsBadBuy'])
+new['IsBadBuy'] = new_y
+new.to_csv('5050_kids_train_cleaned.csv')
